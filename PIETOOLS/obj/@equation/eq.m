db775da2e4055c243ff5c72551f212eb2999945b
Sachin <sachin.shivakumar@asu.edu>
2022-12-07 17:57:44 -0700

    ...

--- PIETOOLS/obj/@equation/eq.m
+++ PIETOOLS/obj/@equation/eq.m
@@ -1,4 +1,10 @@
 function obj = eq(objA,objB)
+if isa(objA,'equation')&&~objA.one_sided
+    error('Incorrect equation format. Cannot process equations of the form A==B==C');
+end
+if isa(objB,'equation')&&~objB.one_sided
+    error('Incorrect equation format. Cannot process equations of the form A==B==C');
+end
 if isa(objA,'state')
     objA = state2equation(objA);
 end
@@ -6,13 +12,20 @@ if isa(objB,'state')
     objB = state2equation(objB);
 end
 if ~isa(objA,'equation')&&(objA==0)
-    lhs.operator = ; lhs.states =; rhs = objB.rhs;
-    obj = equation(lhs,rhs);
+    rhs = objB.rhs;
+    lhs.operator = set(opvarND(),'outlen',length(rhs.operator)); 
+    lhs.states = state(); 
+    obj = equation(rhs,lhs);
 elseif ~isa(objB,'equation')&&(objB==0)
-    obj = objA;
+    objA = -objA;
+    rhs = objA.rhs;
+    lhs.operator = set(opvarND(),'outlen',length(rhs.operator)); 
+    lhs.states = state(); 
+    obj = equation(rhs,lhs);
 elseif isa(objA,'equation')&&isa(objB,'equation')
     obj = objA-objB;
 else
     error('Invalid equation format. Equations must be of the form "expr==0" or "exprA==exprB"');
 end
+obj.one_sided = 0;
 end
--- PIETOOLS/obj/@equation/equation.m
+++ PIETOOLS/obj/@equation/equation.m
@@ -3,16 +3,19 @@ classdef (InferiorClasses={?state})equation
         lhs = struct('operator',opvarND(),'states',state());
         rhs = struct('operator',opvarND(),'states',state());
     end
+    properties (Hidden)
+        one_sided=1;
+    end
     methods
         function obj = equation(varargin)
             if nargin>2
                 error('Terms object constructor takes only two inputs');
             end
-            if nargin==2
-                obj.rhs = varargin{2};
-            end
             if nargin==1
-                obj.lhs = varargin{1};
+                obj.rhs = varargin{1};
+            end
+            if nargin==2
+                obj.lhs = varargin{2};
             end
         end
     end
